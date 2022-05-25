1e1261b27f64e7a2edc0d88f1fbcdfda82067b91
"PatBruckner" <"disoluted1@gmail.com">
2022-05-25 00:13:53 -0400

    require

--- contracts/Ejercicios/Inbox.sol
+++ contracts/Ejercicios/Inbox.sol
@@ -1,23 +1,22 @@
-//SPDX-License-Identifier: GPL-3.0
-
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
 contract Inbox {
 
     string public message;
+    address private ownerAdress;
 
     constructor(string memory initialMessage) {
+        ownerAdress = msg.sender;
         message = initialMessage;
     }
 
-
-    function getMessage()public view returns(string memory){
+    function getMessage() public view returns (string memory) {
         return message;
     }
 
-    function setMessage(string memory newMessage)public{
+    function setMessage(string memory newMessage) public {
+        require(msg.sender == ownerAdress, "Solo el owner puede modificar sus caracteristicas.");
         message = newMessage;
     }
-
-
 }
