e43fcc3bdde68f9817559f286e123b2d9a200847
"PatBruckner" <"disoluted1@gmail.com">
2022-05-25 00:06:15 -0400

    FirstCommit

---
+++ contracts/Ejercicios/Inbox.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract Inbox {
+
+    string public message;
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+
+
+    function getMessage()public view returns(string memory){
+        return message;
+    }
+
+    function setMessage(string memory newMessage)public{
+        message = newMessage;
+    }
+
+
+}
