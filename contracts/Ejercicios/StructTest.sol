a74500445c6e78279172f0d56fc786a2066da317
"PatBruckner" <"disoluted1@gmail.com">
2022-05-31 21:59:02 -0400

    StructTest_mapping

--- contracts/Ejercicios/StructTest.sol
+++ contracts/Ejercicios/StructTest.sol
@@ -7,10 +7,25 @@ contract StructTest {
         string author;
         uint id;
         bool available;
+        uint quantity;
     }
 
     Book public book1;
-    Book public book2 = Book("title2", "ricardo", 2, false);
+    Book public book2 = Book("title2", "ricardo", 2, false, 10);
+
+    mapping(uint => Book) libraryBook;
+
+    function borrowBook(uint idBook) public {
+        libraryBook[idBook].quantity--;
+    }
+
+    function getBook(uint idBook) public view returns(bool) {
+        return libraryBook[idBook].available;
+    }
+
+    function addBook(Book memory newBook) public {
+        libraryBook[newBook.id] = newBook;
+    } 
 
     function getTitle() public view returns(string memory) {
         return book1.title;
