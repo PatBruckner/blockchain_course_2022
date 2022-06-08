8a1604f9eace50b1d7d2977895e63cda56f6759d
Paolo <oeuthfoierughi@gmail.com>
2022-06-08 12:41:52 -0400

    Task 1 08-06-2022: Before each test Lottery

--- ethereum/test/lottery.test.js
+++ ethereum/test/lottery.test.js
@@ -2,13 +2,30 @@ const Lottery = artifacts.require('Lottery');
 
 contract("Lottery",accounts=>{
 
+    let instance;
+
+    beforeEach("Deploy contract from 0",async ()=>{
+         instance = await Lottery.new();
+    })
+
     it("Allows an account to enter lottery", async()=>{
 
-        const instance = await Lottery.deployed();
         await instance.enter({from:accounts[1], value: web3.utils.toWei("3","ether")});
         const players = await instance.getPlayers.call();
         assert.equal(accounts[1],players[0]);
         assert.equal(1,players.length);
+    })
+
+    it("Allows multiple accounts to enter lottery", async()=>{
+
+        await instance.enter({from:accounts[0], value: web3.utils.toWei("1","ether")});
+        await instance.enter({from:accounts[1], value: web3.utils.toWei("1","ether")});
+        await instance.enter({from:accounts[2], value: web3.utils.toWei("1","ether")});
+        const players = await instance.getPlayers.call();
+
+        assert.equal(accounts[0],players[0]);
+        assert.equal(accounts[1],players[1]);
+        assert.equal(accounts[2],players[2]);
     })
 
 })
