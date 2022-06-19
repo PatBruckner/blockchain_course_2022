1e8d43d3798bf48267d13d59492ffb8b3605108e
Paolo <oeuthfoierughi@gmail.com>
2022-06-19 19:13:40 -0400

    [13-06-2022] Task 1 Lottery getPlayers in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {connectWallet, initialize} from "./ethereum/web3";
+import contractLottery from "./ethereum/abis/Lottery.json";
+
 
 function App() {
+
+    const [contract, setContract] = useState<any>('');
+
+    const [manager, setManager] = useState<any>('');
+    const [players, setPlayers] = useState<any>([]);
+    const [balance, setBalance] = useState<any>('');
+
+    const [value, setValue] = useState<any>('');
+    const [message, setMessage] = useState<any>('');
 
     useEffect( ()=> {
         // @ts-ignore
@@ -12,19 +23,53 @@ function App() {
         }
     }, [])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-       <p> Hello React, Truffle, Firebase </p>
-          <button onClick={() => connectWallet()}>Connect</button>
 
-      </header>
-    </div>
-  );
+    const loadBlockchainData = async () => {
+
+        //const contractDeployed;
+
+        // @ts-ignore
+        const Web3 = window.web3;
+
+        // Rinkeby 4, Ganache 5777, BSC 97
+        // @ts-ignore
+        const networkData = contractLottery.networks['5777'];
+        console.log('networkData:', networkData);
+
+        if(networkData) {
+            const abi = contractLottery.abi;
+            const address = networkData.address;
+            console.log('address: ', address);
+            const contractDeployed = new Web3.eth.Contract(abi, address);
+
+            const players = await contractDeployed.methods.getPlayers().call();
+            setPlayers(players);
+            const manager = await contractDeployed.methods.manager().call();
+            setManager(manager)
+            const balance = await Web3.eth.getBalance(contractDeployed.options.address)
+            setBalance(balance)
+
+            setContract(contractDeployed)
+        }
+    }
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <p> Hello React, Truffle, Firebase </p>
+                    <button onClick={() => connectWallet()}>Connect</button>
+
+                    <p>PLAYERS: {players.length}</p>
+                    <p>BALANCE: {balance}</p>
+                    <p>MANAGER: {manager}</p>
+
+                </header>
+            </div>
+        );
+
 }
-
 export default App;
